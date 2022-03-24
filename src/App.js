3b8781fb9d32e8ed3c211d5b555159f86045b95b
guisantosalves <workguisantos@gmail.com>
2022-03-24 16:30:53 -0400

    learning about components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+//quando é exportado por default ele nao precisa estar dentro de chaves
+//quando é exportado sem o default, obrigatoriamente precisa estar dentro de chaves
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
+import Primeiro from './components/Primeiro';
+import X, {Comp2, Comp3} from './components/Multi';
 
-function App(){
-    return <Text>Primeiro componente</Text>
-}
+export default () => (
+    <View>
+        <X/>
+        <Comp2/>
+        <Comp3/>
+        <Primeiro/>
+    </View>
+);
 
-export default App;
---
+++ src/components/Multi.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Text} from 'react-native';
+
+export default function Comp(){
+    return <Text>Usuariooo</Text>
+}
+
+function Comp2(){
+    return <Text>Comp02</Text>
+}
+
+function Comp3(){
+    return <Text>Comp03</Text>
+}
+
+export {Comp2, Comp3}
