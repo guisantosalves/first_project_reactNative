2f82583fae611ed783fa54c5a49eb20e87e2623f
guisantosalves <workguisantos@gmail.com>
2022-03-29 12:24:53 -0400

    gerador de numero aleatorio

--- src/App.js
+++ src/App.js
@@ -22,20 +22,22 @@
 // import FlexboxV1 from './components/layout/FlexboxV1';
 // import FlexboxV2 from './components/layout/FlexboxV2';
 // import FlexboxV3 from './components/layout/FlexboxV3';
+// import FlexboxV4 from './components/layout/FlexboxV4';
 
 import React from 'react';
 import {Text, View, StyleSheet, SafeAreaView} from 'react-native';
-import FlexboxV4 from './components/layout/FlexboxV4';
+import Mega from './components/mega/Mega';
 
 
 export default () => (
     <SafeAreaView style={style.App}>
         
+        <Mega qtdNumeros={12} />
         
-        <FlexboxV4/>
 
         {
         /*
+        <FlexboxV4/>
         <FlexboxV3/>
         <FlexboxV2/>
         <FlexboxV1/> 
---
+++ src/components/mega/Mega.js
@@ -0,0 +1,79 @@
+import React, {Component} from "react";
+import {View, Text, TextInput, Button} from 'react-native';
+import Estilo from '../estilo';
+import MegaNumero from "./MegaNumero";
+
+export default class Mega extends Component{
+
+    state = {
+        qtdNumeros: this.props.qtdNumeros,
+        numeros: []
+    }
+
+    alterarQtdNumero = (qtde)=>{
+        this.setState({qtdNumeros: +qtde})//convertendo para numero
+    }
+
+    //gerar numeros diferentes no array
+    gerarNumeroNaoContido = nums =>{
+        const novo = parseInt(Math.random() * 60) + 1
+        //o método includes() determina de um array contém determinado elemento
+        return nums.includes(novo) ? this.gerarNumeroNaoContido(nums) : novo
+    }
+
+    // gerarNumeros = ()=>{
+    //     const numeros = Array(this.state.qtdNumeros).fill()
+    //         .reduce(n=>[...n, this.gerarNumeroNaoContido(n)], [])
+    //         .sort((a,b)=>a-b)//ordenando de forma crescente
+    //     this.setState({numeros})
+    // }
+
+    gerarNumeros = ()=>{
+        const {qtdNumeros} = this.state
+        const numeros = [];
+
+        for(let i = 0; i<qtdNumeros; i++){
+            const n = this.gerarNumeroNaoContido(numeros)
+            numeros.push(n);
+            
+        }
+        numeros.sort((a, b)=>a-b)
+        this.setState({numeros})
+    }
+
+    exibirNumeros = () =>{
+        const nums = this.state.numeros
+        return nums.map(num => {
+            return <MegaNumero key={num} num={num}/>
+        })
+    }
+
+    render(){
+        return (
+            <>
+                <Text style={Estilo.txtG}>
+                    Gerador de Mega-Sena 
+                </Text>
+                <TextInput 
+                keyboardType={'numeric'}
+                style={{borderBottomWidth:1}}
+                placeholder="qtd de numeros"
+                value={`${this.state.qtdNumeros}`}
+                onChangeText={this.alterarQtdNumero}
+                />
+                <Button 
+                    title="Gerar"
+                    onPress={this.gerarNumeros}
+                />
+                <View style={{
+                marginTop: 20,
+                flexDirection: 'row', 
+                flexWrap: 'wrap', 
+                justifyContent: 'center'}}>
+                    {this.exibirNumeros()}
+                </View>
+                
+            </>
+        )
+    }
+}
